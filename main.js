d219d6181ed8bd2c498300b40751340741ede229
Dom dK <dom@devildog.co.za>
2023-07-25 20:07:40 +0200

    added Javascript

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-var reveal = document.getElementById('star');
+// var reveal = document.getElementById('star');
 
-reveal.addEventListener('click', function (reveal) {
-    reveal.target.classList.toggle('one');
+// reveal.addEventListener('click', function (reveal) {
+//     reveal.target.classList.toggle('one');
 
+// })
+
+const container = document.querySelector('#star1');
+const newImage = document.createElement('img');
+const startOverButton = document.querySelector('#startOver');
+const nextButton = document.querySelector('#next');
+newImage.src = 'star.png';
+
+container.append(newImage);
+
+newImage.addEventListener('click', function () {
+    newImage.src = 'one.png';
+})
+
+startOverButton.addEventListener('click', function () {
+    newImage.src = 'star.png';
+})
+
+nextButton.addEventListener('click', function () {
+    alert('next');
 })
