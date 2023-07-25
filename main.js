ec730b545b3832bd9363b7ed418e481e1cbef05d
Dom dK <dom@devildog.co.za>
2023-07-25 20:41:53 +0200

    new files

--- main.js
+++ main.js
@@ -7,20 +7,33 @@
 
 const container = document.querySelector('#star1');
 const newImage = document.createElement('img');
+const baseURL = '/images/'
 const startOverButton = document.querySelector('#startOver');
 const nextButton = document.querySelector('#next');
-newImage.src = 'star.png';
+// newImage.src = 'images/star.png';
 
-container.append(newImage);
 
-newImage.addEventListener('click', function () {
-    newImage.src = 'one.png';
-})
 
-startOverButton.addEventListener('click', function () {
-    newImage.src = 'star.png';
-})
+// for (let i = 1; i <= 10; i++) {
+//     const star = document.createElement('div');
+//     const newImage = document.createElement('img');
+//     newImage.src = `${baseURL}${i}.png`;
+//     star.appendChild(newImage);
+//     container.append(star);
+// }
 
 nextButton.addEventListener('click', function () {
-    alert('next');
+    for (let i = 1; i <= 1; i++) {
+        const star = document.createElement('div');
+        const newImage = document.createElement('img');
+        newImage.src = `${baseURL}${i}.png`;
+        star.appendChild(newImage);
+        container.append(star);
+    }
+})
+
+
+startOverButton.addEventListener('click', function () {
+
+    newImage.src = `${baseURL}1.png`;
 })
