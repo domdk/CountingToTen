f59ca7ac463c4ad8f4dc375bfbf62325ada5bc22
Dom dK <dom@devildog.co.za>
2023-08-16 12:05:06 +0200

    Javascript count upwards to Ten and reset div when StartOver button Clicked

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
-// var reveal = document.getElementById('star');
 
-// reveal.addEventListener('click', function (reveal) {
-//     reveal.target.classList.toggle('one');
+/*Count the Stars! APP - DomDk*/
 
-// })
-
-const container = document.querySelector('#star1');
+let container = document.querySelector('#allStars');
 const newImage = document.createElement('img');
 const baseURL = '/images/'
 const startOverButton = document.querySelector('#startOver');
 const nextButton = document.querySelector('#next');
-// newImage.src = 'images/star.png';
+let count = 0;
 
+// const star1 = document.getElementById('#star1');
 
+// star1.addEventListener('click', function () {
+//     let number = document.createElement('p');
+//     star1.append(number);
+//     number = "1";
+// })
 
-// for (let i = 1; i <= 10; i++) {
-//     const star = document.createElement('div');
-//     const newImage = document.createElement('img');
-//     newImage.src = `${baseURL}${i}.png`;
-//     star.appendChild(newImage);
-//     container.append(star);
-// }
 
 nextButton.addEventListener('click', function () {
     for (let i = 1; i <= 1; i++) {
-        const star = document.createElement('div');
-        const newImage = document.createElement('img');
-        newImage.src = `${baseURL}${i}.png`;
-        star.appendChild(newImage);
-        container.append(star);
+
+        if (count <= 9) {
+            const star = document.createElement('div');
+            const newImage = document.createElement('img');
+            count++;
+            newImage.src = `${baseURL}${count}.png`;
+            star.appendChild(newImage);
+            container.append(star);
+            nextButton.innerText = (count);
+        }
     }
 })
 
 
 startOverButton.addEventListener('click', function () {
-
-    newImage.src = `${baseURL}1.png`;
+    container.innerHTML = "";
+    nextButton.innerText = "Next";
+    count = 0;
 })
+
